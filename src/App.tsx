6aac8945600e251dfac4337ef81ab595c0d25c30
William <mauritius.william.wijaya1@gmail.com>
2023-09-12 22:09:27 +0700

    adding move and change scramble logic and add arrow

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import './App.scss'
 import Stopwatch from './stopwatch';
 
@@ -8,7 +8,9 @@ function App() {
   const [activeIndex, setActiveIndex] = useState<number[]>([])
   const [isWin, setIsWin] = useState<Boolean>(false)
   const [resetStopwatch, setResetStopwatch] = useState<boolean>(false)
+  const [countMove, setCountMove] = useState<number>(0)
 
+  console.log(activeIndex)
   function scramble() {
     let array = [
       [1, 2, 3, 4],
@@ -17,53 +19,53 @@ function App() {
       [13, 14, 15, '']
     ]
 
-    let i = 50
+    let i = 100
     while (i > 0) {
       const random = Math.ceil(Math.random() * 4),
         findEmpty = array.flat().findIndex(el => el === ''),
         indexY = Math.floor(findEmpty / 4),
         indexX = findEmpty % 4
-      switch (random) {
-        case 1:
-          if (array[indexY][indexX - 1]) {
-            [array[indexY][indexX - 1], array[indexY][indexX]] = [array[indexY][indexX], array[indexY][indexX - 1]]
-            i--
-          }
-          break;
-        case 2:
-          if (array[indexY][indexX + 1]) {
-            [array[indexY][indexX + 1], array[indexY][indexX]] = [array[indexY][indexX], array[indexY][indexX + 1]]
-            i--
-          }
-          break;
-        case 3:
-          if (array[indexY - 1][indexX]) {
-            [array[indexY - 1][indexX], array[indexY][indexX]] = [array[indexY][indexX], array[indexY - 1][indexX]]
-            i--
-          }
-          break;
-        case 4:
-          if (array[indexY + 1][indexX]) {
-            [array[indexY + 1][indexX], array[indexY][indexX]] = [array[indexY][indexX], array[indexY + 1][indexX]]
-            i--
-          }
-          break;
-        default:
-          break;
+
+      if (random === 1) {
+        if (array[indexY][indexX - 1]) {
+          [array[indexY][indexX - 1], array[indexY][indexX]] = [array[indexY][indexX], array[indexY][indexX - 1]]
+          i--
+        }
+      } else if (random === 2) {
+        if (array[indexY][indexX + 1]) {
+          [array[indexY][indexX + 1], array[indexY][indexX]] = [array[indexY][indexX], array[indexY][indexX + 1]]
+          i--
+        }
+      } else if (random === 3) {
+        if (array[indexY - 1]) {
+          [array[indexY - 1][indexX], array[indexY][indexX]] = [array[indexY][indexX], array[indexY - 1][indexX]]
+          i--
+        }
+      } else if (random === 4) {
+        if (array[indexY + 1]) {
+          [array[indexY + 1][indexX], array[indexY][indexX]] = [array[indexY][indexX], array[indexY + 1][indexX]]
+          i--
+        }
       }
     }
+    const findEmpty = array.flat().findIndex(el => el === ''),
+      indexY = Math.floor(findEmpty / 4),
+      indexX = findEmpty % 4
+    setActiveIndex([indexY, indexX])
     setPuzzle(array)
   }
 
-  console.log(puzzle)
+  const containerRef: any = useRef(null);
 
   useEffect(() => {
     scramble()
+    containerRef.current?.focus();
   }, [])
 
   function handleclick(index1: number, index2: number): void {
     if (isWin) return
     let puzzleArea = [...puzzle]
+    setCountMove(prevState => prevState + 1)
 
     function move(direction: 'top' | 'left' | 'right' | 'bottom') {
       setActiveIndex([index1, index2])
@@ -85,7 +87,7 @@ function App() {
         }
         setPuzzle(updatedPuzzle);
         setDirection('')
-      }, 100);
+      }, 50);
 
       return () => {
         clearTimeout(timer);
@@ -104,6 +106,16 @@ function App() {
     else if (puzzleArea[index1] && typeof puzzleArea[index1][index2 - 1] === 'string') {
       move('left')
     }
+  }
+
+  function handleKeyPress(index1: number, index2: number, e: any): void {
+    var key = e.key;
+    if (key === 'ArrowUp') index1 += 1
+    else if (key === 'ArrowDown') index1 -= 1
+    else if (key === 'ArrowLeft') index2 += 1
+    else if (key === 'ArrowRight') index2 -= 1
+    if (index2 > 3 || index2 < 0 || index1 > 3 || index1 < 0) return
+    handleclick(index1, index2)
   }
 
   function reset(): void {
@@ -127,6 +139,24 @@ function App() {
     }
   }, [puzzle]);
 
+  useEffect(() => {
+    const handleDocumentKeyDown = (e: any) => {
+      if (isWin) return; // Don't handle key events when the game is won
+      if (e.key === 'ArrowUp' || e.key === 'ArrowDown' || e.key === 'ArrowLeft' || e.key === 'ArrowRight') {
+        e.preventDefault(); // Prevent browser scrolling with arrow keys
+        const index1 = activeIndex[0];
+        const index2 = activeIndex[1];
+        handleKeyPress(index1, index2, e);
+      }
+    };
+
+    document.addEventListener('keydown', handleDocumentKeyDown);
+
+    return () => {
+      document.removeEventListener('keydown', handleDocumentKeyDown);
+    };
+  }, [activeIndex, isWin]);
+
   return (
     <div className='center'>
       <h1>STACKUN</h1>
@@ -137,10 +167,10 @@ function App() {
               if (isWin) {
                 if (typeof el1 === 'string') return <span key={el1}>&#8862;</span>
               } else {
-                if (typeof el1 === 'string') return <span key={el1} className='empty-space'></span>
+                if (typeof el1 === 'string') return <input key={el1} className='empty-space' onKeyDown={(e) => handleKeyPress(index1, index2, e)} tabIndex={0} autoFocus />
               }
               if (index1 === activeIndex[0] && index2 === activeIndex[1]) {
-                return <span key={el1} className={direction} onClick={() => handleclick(index1, index2)}>{el1}</span>
+                return <span key={el1} className={direction} onClick={() => handleclick(index1, index2)} ref={containerRef}>{el1}</span>
               }
               return <span key={el1} onClick={() => handleclick(index1, index2)}>{el1}</span>
             })
@@ -149,7 +179,16 @@ function App() {
       </div>
       <div className="bottom-content">
         <Stopwatch reset={resetStopwatch} isRunning={!isWin} />
+        <div className='stopwatch'>Move: {countMove}</div>
         {isWin && <button onClick={reset}>Reset</button>}
+      </div>
+      <div className="joy-stick">
+        <span style={{ transform: 'rotate(180deg)' }} onClick={() => handleKeyPress(activeIndex[0], activeIndex[1], { key: 'ArrowLeft' })}>&#10148;</span>
+        <div>
+          <span style={{ transform: 'rotate(-90deg)' }} onClick={() => handleKeyPress(activeIndex[0], activeIndex[1], { key: 'ArrowTop' })}>&#10148;</span>
+          <span style={{ transform: 'rotate(90deg)' }} onClick={() => handleKeyPress(activeIndex[0], activeIndex[1], { key: 'ArrowDown' })}>&#10148;</span>
+        </div>
+        <span onClick={() => handleKeyPress(activeIndex[0], activeIndex[1], { key: 'ArrowRight' })}>&#10148;</span>
       </div>
 
     </div>
